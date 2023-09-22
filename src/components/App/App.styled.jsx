c4736a92c2c4268ea90aa50fbb96041cea9722d3
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-22 23:49:02 +0300

    fix

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -4,7 +4,7 @@ export const Container = styled.div`
     width: 500px;
     margin: 0 auto;
     padding: 20px;
-    background-color: blue;
+    background-color: #7373a1;
     border-radius: 6px;
     
 `;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,57 +1,67 @@
 import { Form, FormLabel, FormInput, FormButton } from './ContactForm.styled';
 import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from 'redux/operations';
 import { getContacts } from 'redux/selectors';
 import Notiflix from 'notiflix';
-import { addContact } from 'redux/operations';
-
-
 
 export function ContactForm() {
-    const { contactsItem } = useSelector(getContacts);
-    const dispatch = useDispatch();
+  const { contactsItem } = useSelector(getContacts);
+  const dispatch = useDispatch();
 
-    const handleSubmit = evt => {
-        evt.preventDefault();
-        const form = evt.target;
-        const { name, number } = form.elements;
-        
-        if (contacts.some(contact => contact.name.toLowerCase() === name.value.toLowerCase())) {
-          Notiflix.Report.warning(
-            'Warning',
-            `${name.value} is already in contacts.`
-          );
-        } else {
-            dispatch(addContact({ name: name.value, phone: number.value }));
-        }
-        form.reset();
-    };
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    const form = evt.target;
+    const { name, number } = form.elements;
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <FormLabel>
-                Name
-                <FormInput
-                    type='text'
-                    name='name'
-                    pattern="^[a-zA-Zа-яА-Я\s]+$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                />
-            </FormLabel>
-            <FormLabel>
-                Number
-                <FormInput
-                    type="tel"
-                    name="number"
-                    pattern="\+?[0-9\s\-\(\)]+"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                />
-            </FormLabel>
-            <FormButton type='submit'>Add Contact</FormButton>              
-        </Form>
-    );
+    if (contactsItem.some(contact => contact.name === name.value)) {
+      Notiflix.Report.warning(
+        'Warning',
+        `${name.value} is already in contacts.`
+      );
+    } else {
+      dispatch(addContact({ name: name.value, phone: number.value }));
+      //
+    }
+    form.reset();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormLabel>
+        Name
+        <FormInput
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я\s]+$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </FormLabel>
+      <FormLabel>
+        Number
+        <FormInput
+          type="tel"
+          name="number"
+          pattern="\+?[0-9\s\-\(\)]+"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </FormLabel>
+      <FormButton type="submit">Add contact</FormButton>
+    </Form>
+  );
 }
+
+Notiflix.Report.init({
+  svgSize: '50px',
+  messageFontSize: '20px',
+  warning: {
+    svgColor: '#44728f',
+
+    buttonBackground: '#44728f',
+    backOverlayColor: 'rgba(0,0, 0,0.2)',
+  },
+});
 
 
 
