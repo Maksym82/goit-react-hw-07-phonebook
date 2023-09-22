ca64eb1e70baf9db8e64a753925365206e0d5374
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-22 22:49:00 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,8 @@ import { filterReducer } from './filterSlice';
 import { contactReducer } from './contactsSlice';
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactReducer,
-        filter: filterReducer,
-    },
+  reducer: {
+    contacts: contactReducer,
+    filter: filterReducer,
+  },
 });
-
