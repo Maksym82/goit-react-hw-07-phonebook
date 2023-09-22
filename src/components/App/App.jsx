c42c34307b493812aaec89ec95be343eabaa9fc3
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-09-22 21:18:24 +0300

    update app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,37 @@
-import { ContactForm } from '../ContactForm/ContactForm';
-import { Filter } from '../Filter/Filter';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getContacts } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
-import { Section } from '../Section/Section';
+import { Section } from 'components/Section/Section';
 import { Container } from './App.styled';
 
-  
+export function App() {
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(getContacts);
 
- export function App() {
+  useEffect(() =>{
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <Container>
       <Section title={'Phonebook'}>
         <ContactForm />
       </Section>
+      {isLoading && !error && <b>Request in progress...</b>}
       <Section title={'Contacts'}>
         <Filter />
         <ContactList />
       </Section>
     </Container>
   );
- }
+}
 
-  
- 
 
- 
 
- 
+
+
+
